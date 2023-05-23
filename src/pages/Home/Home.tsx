8333c606306c3cc6a5d537a4a4065d69c62157d9
Iarlen <iarlensr@hotmail.com>
2023-05-23 17:45:23 -0300

    feat: adding the FemaleSection component on the Home page.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import BannerHero from '../../components/BannerHero/BannerHero'
 import Benefits from '../../components/Benefits/Benefits'
+import FemaleSection from '../../components/FemaleSection/FemaleSection'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Header />
       <BannerHero />
       <Benefits />
+      <FemaleSection />
     </>
   )
 }
