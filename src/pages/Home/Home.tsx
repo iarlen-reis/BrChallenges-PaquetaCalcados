6150c559f249ad0f320f1f1b877fa4c146c75a8f
Iarlen <iarlensr@hotmail.com>
2023-05-23 16:34:27 -0300

    feat: adding the benefit component on the Home page.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import BannerHero from '../../components/BannerHero/BannerHero'
+import Benefits from '../../components/Benefits/Benefits'
 
 const Home = () => {
   return (
     <>
       <Header />
       <BannerHero />
+      <Benefits />
     </>
   )
 }
