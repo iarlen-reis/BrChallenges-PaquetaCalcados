7d7e6f2f36d312ac9cfef0e8c3fea86ab9852326
Iarlen <iarlensr@hotmail.com>
2023-05-23 18:40:45 -0300

    feat: adding the MaleSection component on the Home page.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import Header from '../../components/Header/Header'
 import BannerHero from '../../components/BannerHero/BannerHero'
 import Benefits from '../../components/Benefits/Benefits'
 import FemaleSection from '../../components/FemaleSection/FemaleSection'
+import MaleSection from '../../components/MaleSection/MaleSection'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <BannerHero />
       <Benefits />
       <FemaleSection />
+      <MaleSection />
     </>
   )
 }
