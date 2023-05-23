923a1d390d7fa6355ff3c1095510c9e68b352390
Iarlen <iarlensr@hotmail.com>
2023-05-22 23:41:25 -0300

    refactor: moving routes to main.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import Home from './pages/Home/Home'
+import { Theme } from './styles/theme/theme'
+import GlboalStyled from './styles/Global'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Word!</h1>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={Theme}>
+        <GlboalStyled />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
 
-import GlboalStyled from './styles/Global'
-import { Theme } from './styles/theme/theme'
-import { ThemeProvider } from 'styled-components'
-
-import { router } from './routes/routes'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={Theme}>
-      <GlboalStyled />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
 )
