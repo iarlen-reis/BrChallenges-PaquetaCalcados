739e815cbf3683744937b76e94997eb705f32451
Iarlen <iarlensr@hotmail.com>
2023-05-24 03:26:59 -0300

    feat: creating Advertisers component and adding styles.

---
+++ src/components/Advertisers/Advertisers.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-absolute-path */
+import React from 'react'
+
+import { Container } from '../../styles/Global'
+
+import Adidas from '/Images/Adidas.png'
+import ViaMarte from '/Images/ViaMarte.png'
+import Bebece from '/Images/Bebece.png'
+import Dumond from '/Images/Dumond.png'
+
+import { Advertiserstyled } from './styles'
+
+const Advertisers = () => {
+  return (
+    <Container>
+      <Advertiserstyled>
+        <h2>AS MELHORES MARCAS ESTÃO AQUI!</h2>
+        <ul>
+          <li>
+            <img src={Adidas} alt="Logo da marca Adidas" />
+          </li>
+          <li>
+            <img src={ViaMarte} alt="Logo da marca ViaMarte" />
+          </li>
+          <li>
+            <img src={Bebece} alt="Logo da marca Bebecê" />
+          </li>
+          <li>
+            <img src={Dumond} alt="Logo da marca Dumond" />
+          </li>
+        </ul>
+      </Advertiserstyled>
+    </Container>
+  )
+}
+
+export default Advertisers
---
+++ src/components/Advertisers/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Advertiserstyled = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  gap: 8rem;
+
+  margin-top: 16.8rem;
+
+  h2 {
+    text-align: left;
+    font-family: ${({ theme }) => theme.font.montserrat};
+    font-size: 3.4rem;
+    line-height: 4.76rem;
+    font-weight: 600;
+
+    color: ${({ theme }) => theme.colors.SecundaryColor};
+  }
+
+  ul {
+    width: 100%;
+    display: flex;
+    align-items: baseline;
+    justify-content: space-between;
+    list-style: none;
+  }
+`
