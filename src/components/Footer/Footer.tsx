70989e7ed7c669bbe7e27674d2dcfe49eb0c2ae6
Iarlen <iarlensr@hotmail.com>
2023-05-24 22:32:30 -0300

    feat: creating Footer component and adding styles.

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,172 @@
+/* eslint-disable import/no-absolute-path */
+import React from 'react'
+
+import MasterCard from '/Images/Footer/MasterCard.png'
+import AmericanExpress from '/Images/Footer/AmericaExpress.png'
+import Elo from '/Images/Footer/Elo.png'
+import Paqueta from '/Images/Footer/Paqueta.png'
+import Gaston from '/Images/Footer/Gaston.png'
+import Visa from '/Images/Footer/Visa.png'
+import HiperCard from '/Images/Footer/HiperCard.png'
+import Itau from '/Images/Footer/Itau.png'
+import Boleto from '/Images/Footer/Boleto.png'
+import Pinterest from '/Images/Footer/Pinterest.png'
+import Award from '/Images/Footer/Award.png'
+import Blindado from '/Images/Footer/Blindado.png'
+
+import { FiFacebook, FiTwitter, FiYoutube, FiInstagram } from 'react-icons/fi'
+
+import {
+  FooterStyled,
+  FooterContainer,
+  ColumnFooterStyled,
+  SpecialColumnFooterStyled,
+  PaymentColumnStyled,
+  SocialColumnStyled,
+  LastColumnStyled,
+} from './styles'
+
+const Footer = () => {
+  return (
+    <FooterStyled>
+      <FooterContainer>
+        <ColumnFooterStyled>
+          <li>
+            <h3>Institucional</h3>
+          </li>
+          <li>
+            <a href="#">Quem Somos</a>
+          </li>
+          <li>
+            <a href="#">Nossas Lojas</a>
+          </li>
+          <li>
+            <a href="#">Soluções Corporativas</a>
+          </li>
+          <li>
+            <a href="#">Política de Privacidade</a>
+          </li>
+          <li>
+            <a href="#">Contrato de Compra e Venda</a>
+          </li>
+          <li>
+            <a href="#">Crédito Pessoal</a>
+          </li>
+        </ColumnFooterStyled>
+
+        <ColumnFooterStyled>
+          <li>
+            <h3>Ajuda e Suporte</h3>
+          </li>
+          <li>
+            <a href="#">Troca e Devolução</a>
+          </li>
+          <li>
+            <a href="#">2ª Via do Boleto</a>
+          </li>
+          <li>
+            <a href="#">Detalhamento de Fatura</a>
+          </li>
+          <li>
+            <a href="#">Entregas</a>
+          </li>
+          <li>
+            <a href="#">Meus Pedidos</a>
+          </li>
+          <li>
+            <a href="#">Dúvidas Frequentes</a>
+          </li>
+          <li>
+            <a href="#">Status do Pedido</a>
+          </li>
+        </ColumnFooterStyled>
+
+        <SpecialColumnFooterStyled>
+          <div>
+            <h3>Atendimento</h3>
+            <a href="#">Fale Conosco</a>
+          </div>
+          <div>
+            <h3>Endereço</h3>
+            <a href="#">
+              Rua Antonio Frederico Ozanan, 2601 Canoas - RS | CNPJ:
+              01.098.983/0175-02
+            </a>
+          </div>
+          <div>
+            <h3>Trabalhe Conosco</h3>
+            <a href="#">Venha crescer com a Paquetá!</a>
+          </div>
+        </SpecialColumnFooterStyled>
+        <PaymentColumnStyled>
+          <h3>Formas de Pagamento</h3>
+          <ul>
+            <li>
+              <img src={MasterCard} alt=" logo da mastercard" />
+            </li>
+            <li>
+              <img src={AmericanExpress} alt=" logo da AmericanExpress" />
+            </li>
+            <li>
+              <img src={Elo} alt=" logo da Elo" />
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <img src={Paqueta} alt=" logo da Paqueta" />
+            </li>
+            <li>
+              <img src={Gaston} alt=" logo da Gaston" />
+            </li>
+            <li>
+              <img src={Visa} alt=" logo da Visa" />
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <img src={HiperCard} alt=" logo da HiperCard" />
+            </li>
+            <li>
+              <img src={Itau} alt=" logo da Itau" />
+            </li>
+            <li>
+              <img src={Boleto} alt="icone de um Boleto" />
+            </li>
+          </ul>
+          <SocialColumnStyled>
+            <h3>Redes Sociais</h3>
+            <ul>
+              <li>
+                <FiInstagram />
+              </li>
+              <li>
+                <FiFacebook />
+              </li>
+              <li>
+                <FiTwitter />
+              </li>
+              <li>
+                <FiYoutube />
+              </li>
+              <li>
+                <img src={Pinterest} alt="logo da pinterest" />
+              </li>
+            </ul>
+          </SocialColumnStyled>
+        </PaymentColumnStyled>
+        <LastColumnStyled>
+          <div>
+            <h3>Prêmios e Certificações</h3>
+            <img src={Award} alt="prêmios e certificações" />
+          </div>
+          <div>
+            <h3>Segurança</h3>
+            <img src={Blindado} alt="Segurança" />
+          </div>
+        </LastColumnStyled>
+      </FooterContainer>
+    </FooterStyled>
+  )
+}
+
+export default Footer
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,175 @@
+import styled from 'styled-components'
+
+export const FooterStyled = styled.footer`
+  width: 100%;
+  height: 34.9rem;
+`
+
+export const FooterContainer = styled.div`
+  max-width: 1300px;
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+
+  align-items: start;
+  justify-content: space-between;
+
+  margin-top: 5.8rem;
+`
+
+export const ColumnFooterStyled = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+
+  text-align: left;
+  list-style: none;
+
+  h3 {
+    font-family: ${({ theme }) => theme.font.montserrat};
+    font-size: 1.8rem;
+    line-height: 2.8rem;
+    font-weight: 700;
+
+    color: ${({ theme }) => theme.colors.SecundaryColorUnder};
+  }
+
+  a {
+    font-family: ${({ theme }) => theme.font.montserrat};
+    font-size: 1.4rem;
+    line-height: 2.24rem;
+
+    text-decoration: none;
+
+    opacity: 0.6;
+
+    color: ${({ theme }) => theme.colors.SecundaryColorUnder};
+  }
+`
+
+export const SpecialColumnFooterStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 3.8rem;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+
+    gap: 1.2rem;
+  }
+
+  h3 {
+    font-family: ${({ theme }) => theme.font.montserrat};
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 28px;
+
+    color: ${({ theme }) => theme.colors.SecundaryColorUnder};
+  }
+
+  a {
+    width: 28.2rem;
+    font-family: ${({ theme }) => theme.font.montserrat};
+    font-size: 1.4rem;
+    line-height: 2.2rem;
+
+    text-decoration: none;
+
+    opacity: 0.6;
+
+    color: ${({ theme }) => theme.colors.SecundaryColorUnder};
+  }
+`
+
+export const PaymentColumnStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  h3 {
+    font-family: ${({ theme }) => theme.font.montserrat};
+    font-size: 1.8rem;
+    line-height: 2.8rem;
+    font-weight: 700;
+
+    color: ${({ theme }) => theme.colors.SecundaryColorUnder};
+  }
+
+  ul {
+    display: flex;
+    gap: 2rem;
+
+    list-style: none;
+  }
+
+  svg {
+    font-size: 2.4rem;
+
+    color: ${({ theme }) => theme.colors.SecundaryColorUnder};
+  }
+`
+export const SocialColumnStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  list-style: none;
+
+  h3 {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 28px;
+
+    color: ${({ theme }) => theme.colors.SecundaryColorUnder};
+  }
+
+  ul {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+`
+
+export const LastColumnStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+
+  text-align: left;
+
+  gap: 7rem;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+
+    gap: 1rem;
+  }
+
+  div:nth-child(1) {
+    img {
+      width: 70px;
+      height: 95px;
+    }
+  }
+
+  div:nth-child(2) {
+    img {
+      width: 115px;
+      height: 32px;
+    }
+  }
+
+  h3 {
+    font-family: ${({ theme }) => theme.font.montserrat};
+    font-size: 18px;
+    line-height: 28px;
+    font-weight: 700;
+
+    color: ${({ theme }) => theme.colors.SecundaryColorUnder};
+  }
+`
