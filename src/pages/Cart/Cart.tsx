c51166e5aa747e52271c425474ecb331072b0011
Iarlen <iarlensr@hotmail.com>
2023-05-28 16:22:57 -0300

    feat: adding slide effect on Cart page.

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -20,6 +20,8 @@ import { FiTrash2 } from 'react-icons/fi'
 
 import { useCartContext } from '../../context/CartContext'
 
+import { Slide } from 'react-awesome-reveal'
+
 const Cart = () => {
   const { cart, removeItemCart } = useCartContext()
 
@@ -35,44 +37,53 @@ const Cart = () => {
         </NavigationStyled>
         <TitleCartStyled>
           <h1>Sacola de compras</h1>
-          <h2>Valor total: R$: {total.toFixed(2)}</h2>
+          <Slide direction="right">
+            <h2>Valor total: R$: {total.toFixed(2)}</h2>
+          </Slide>
         </TitleCartStyled>
         <UlStyled>
           <h2>Itens</h2>
           {cart.length > 0 ? (
             cart.map((cart, index) => (
-              <li key={index}>
-                <img src={cart.image} alt="" />
-                <ContentStyled>
-                  <DescriptionStyled>
-                    <TitleStyled>
-                      <Link to={`/shoe/${cart.id}`}>{cart.name}</Link>
-                      <span>Código do produto: {cart.productCode}</span>
-                    </TitleStyled>
-                    <DetailStyled>
-                      <p>
-                        <span>Numeração:</span> {cart.shoeSize}
-                      </p>
-                      <p>
-                        <span>Cor:</span> Preto
-                      </p>
-                      <p>
-                        <span>Quantidade:</span> {cart.amount}
-                      </p>
-                      <p>
-                        <span>Preço:</span> R${' '}
-                        {(Number(cart.price) * cart.amount).toFixed(2)}
-                      </p>
-                    </DetailStyled>
-                  </DescriptionStyled>
-                  <RemoveStyled
-                    onClick={() => removeItemCart(cart.id, cart.shoeSize)}
-                  >
-                    <FiTrash2 />
-                    Remover
-                  </RemoveStyled>
-                </ContentStyled>
-              </li>
+              <Slide
+                key={index}
+                direction="right"
+                duration={1500}
+                triggerOnce={true}
+              >
+                <li>
+                  <img src={cart.image} alt="" />
+                  <ContentStyled>
+                    <DescriptionStyled>
+                      <TitleStyled>
+                        <Link to={`/shoe/${cart.id}`}>{cart.name}</Link>
+                        <span>Código do produto: {cart.productCode}</span>
+                      </TitleStyled>
+                      <DetailStyled>
+                        <p>
+                          <span>Numeração:</span> {cart.shoeSize}
+                        </p>
+                        <p>
+                          <span>Cor:</span> Preto
+                        </p>
+                        <p>
+                          <span>Quantidade:</span> {cart.amount}
+                        </p>
+                        <p>
+                          <span>Preço:</span> R${' '}
+                          {(Number(cart.price) * cart.amount).toFixed(2)}
+                        </p>
+                      </DetailStyled>
+                    </DescriptionStyled>
+                    <RemoveStyled
+                      onClick={() => removeItemCart(cart.id, cart.shoeSize)}
+                    >
+                      <FiTrash2 />
+                      Remover
+                    </RemoveStyled>
+                  </ContentStyled>
+                </li>
+              </Slide>
             ))
           ) : (
             <NoCartItems>
