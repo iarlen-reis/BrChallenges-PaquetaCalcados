fe572152c885cbe846da6381a924f374885507cf
Iarlen <iarlensr@hotmail.com>
2023-05-29 19:36:08 -0300

    feat: adding animation on Favorites page.

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -14,6 +14,8 @@ import {
 
 import { FiTrash2 } from 'react-icons/fi'
 
+import { Slide } from 'react-awesome-reveal'
+
 import { useFavoriteContext } from '../../context/FavoriteContext'
 
 const Favorites = () => {
@@ -34,24 +36,26 @@ const Favorites = () => {
           <h2>Favoritos</h2>
           {favorites.length > 0 ? (
             favorites.map((item) => (
-              <li key={item.id}>
-                <img src={item.image} alt={item.name} />
-                <DescriptionStyled>
-                  <div>
-                    <h2>{item.name}</h2>
-                    <p>codigo do produto</p>
-                  </div>
-                  <p>
-                    <span>Preço:</span> R$ {item.price}
-                  </p>
-                </DescriptionStyled>
-                <ButtonRemoveStyled
-                  onClick={() => randleRemoveToFavorite(item.id)}
-                >
-                  <FiTrash2 />
-                  Remover
-                </ButtonRemoveStyled>
-              </li>
+              <Slide key={item.id} direction="right" triggerOnce={true}>
+                <li>
+                  <img src={item.image} alt={item.name} />
+                  <DescriptionStyled>
+                    <div>
+                      <h2>{item.name}</h2>
+                      <p>codigo do produto</p>
+                    </div>
+                    <p>
+                      <span>Preço:</span> R$ {item.price}
+                    </p>
+                  </DescriptionStyled>
+                  <ButtonRemoveStyled
+                    onClick={() => randleRemoveToFavorite(item.id)}
+                  >
+                    <FiTrash2 />
+                    Remover
+                  </ButtonRemoveStyled>
+                </li>
+              </Slide>
             ))
           ) : (
             <NoFavorite>
